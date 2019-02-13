5f06e3004b4faf573ef54d8780525f059e7d4587
Gift <laurandidi21@gmailcom>
2019-02-13 10:27:35 +0100

    Add email to list

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,10 +2,17 @@
   <Layout>
     <div class="content">
       <div class="card" v-for="edge in $page.allFaker.edges" :key="edge.node.id">
-        <div class="card-content">
+        <div class="card-content columns">
+        <div class="column is-2">
+          <figure class="image is-128x128">
+            <img class="is-rounded" :src="edge.node.avatar.src">
+          </figure>
+        </div>
+        <div class="column">
           <p class="title"> {{ edge.node.author }}</p>
-          <p class="subtitle"> <i>Contact Email:</i> {{ edge.node.email.src }}</p>
-          <p class="subtitle"> <i> Profile: </i> {{ edge.node.title }}</p>
+          <p class="subtitle"> <i class="has-text-dark">Contact Email:</i> {{ edge.node.email.src }}</p>
+          <p class="subtitle"> <i class="has-text-dark"> Profile: </i> {{ edge.node.title }}</p>
+        </div>
         </div>
       </div>
     </div>
